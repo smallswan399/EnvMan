735f799956e7c44718647bf2b65b1f8850fc0313
v_setchin <v_setchin@88464f1c-ca2d-0410-b414-c3b21fb8b5ff>
2007-12-27 19:14:53 +0000

    Updated E-Mail Addresses in licence comments

--- EnvMan/branches/BT1795598_AutoUpdatesManager/AUM/AUM/Properties/AssemblyInfo.cs
+++ EnvMan/branches/BT1795598_AutoUpdatesManager/AUM/AUM/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿/*
    AUM - Automated Updates Manager
-   Copyright (C) 2006-2007 Vlad Setchin <v_setchin@yahoo.com.au>
+   Copyright (C) 2006-2007 Vlad Setchin <auto.updates.mng@gmail.com>
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
