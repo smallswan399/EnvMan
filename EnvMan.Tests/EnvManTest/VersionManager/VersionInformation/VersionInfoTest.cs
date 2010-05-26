f3a2007e77a9c8510bf2e00d558b5de41e1a9d42
v_setchin <v_setchin@88464f1c-ca2d-0410-b414-c3b21fb8b5ff>
2010-05-26 10:10:42 +0000

    Made Merge with Auto-Updates Manager branch (RC1) Checked in Test Project for EnvMan

---
+++ EnvMan.Tests/EnvManTest/VersionManager/VersionInformation/VersionInfoTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using NUnit.Framework;
+using EnvMan.VersionManager.VersionInformation;
+
+namespace EnvMan.Tests.VersionManager.VersionInformation
+{
+    [TestFixture]
+    public class VersionInfoTest
+    {
+        [Test]
+        public void TestVersionFormatter1_4RC1()
+        {
+            string versionString = VersionInfo.VersionFormatter(new Version(1, 4, 0, 1));
+
+            Assert.AreEqual("V1.4 RC1", versionString);
+        }
+
+        [Test]
+        public void TestVersionFormatter1_4()
+        {
+            string versionString = VersionInfo.VersionFormatter(new Version(1, 4, 0, 0));
+
+            Assert.AreEqual("V1.4", versionString);
+        }
+    }
+}
