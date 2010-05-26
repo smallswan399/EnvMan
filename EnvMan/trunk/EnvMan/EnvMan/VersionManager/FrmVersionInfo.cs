0f72424649b2131719b13e69705f6a9963b24781
v_setchin <v_setchin@88464f1c-ca2d-0410-b414-c3b21fb8b5ff>
2010-05-26 10:13:55 +0000

    Made Merge with Auto-Updates Manager branch (RC1) adding missing folders

---
+++ EnvMan/trunk/EnvMan/EnvMan/VersionManager/FrmVersionInfo.cs
@@ -0,0 +1,95 @@
+/*
+   EnvMan - The Open-Source Windows Environment Variables Manager
+   Copyright (C) 2006-2009 Vlad Setchin <envman-dev@googlegroups.com>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EnvMan.VersionManager
+{
+    public partial class FrmVersionInfo : Form
+    {
+        public FrmVersionInfo ( )
+        {
+            InitializeComponent();
+
+            this.Text = "EnvMan - New version detected";
+        }
+
+        /// <summary>
+        /// Sets the message.
+        /// </summary>
+        /// <value>The message.</value>
+        public string Message
+        {
+            set { lblMessage.Text = value; }
+        }
+
+        /// <summary>
+        /// BTNs the click.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+#if DEBUG
+        public void BtnClick ( object sender, EventArgs e )
+#else
+        private void BtnClick ( object sender, EventArgs e ) 
+#endif
+        {
+            if (sender.Equals(btnCancel))
+            {
+                this.DialogResult = DialogResult.Cancel;
+            }
+            else if (sender.Equals(btnOK))
+            {
+                this.DialogResult = DialogResult.OK;
+            }
+            this.Hide();
+        }
+	
+#if DEBUG
+        /// <summary>
+        /// Gets the OK Button.
+        /// </summary>
+        /// <value>The OK Button.</value>
+        public Button BtnOK
+        {
+            get
+            {
+                return this.btnOK;
+            }
+        }
+
+        /// <summary>
+        /// Gets the Cancel Button.
+        /// </summary>
+        /// <value>The Cancel Button.</value>
+        public Button BtnCancel
+        {
+            get
+            {
+                return this.btnCancel;
+            }
+        }
+#endif
+    }
+}
---
+++ EnvMan/trunk/EnvMan/EnvMan/VersionManager/VersionInformation/VersionInfoManager.cs
@@ -0,0 +1,86 @@
+/*
+   EnvMan - The Open-Source Windows Environment Variables Manager
+   Copyright (C) 2006-2009 Vlad Setchin <envman-dev@googlegroups.com>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace EnvMan.VersionManager.VersionInformation
+{
+    /// <summary>
+    /// Class for Saving and Loading Version Info from File
+    /// </summary>
+    public class VersionInfoManager
+    {
+        private VersionInfo versionInfo = new VersionInfo();
+        /// <summary>
+        /// Gets or sets the version information.
+        /// </summary>
+        /// <value>The version information.</value>
+        public VersionInfo VersionInformation
+        {
+            set
+            {
+                this.versionInfo = value;
+            }
+            get
+            {
+                return this.versionInfo;
+            }
+        }
+        /// <summary>
+        /// Saves VersionInfo into specified file.
+        /// </summary>
+        /// <param name="filename">The filename.</param>
+        public void Save ( string filename )
+        {
+            //Create a file stream object
+            FileStream file = File.Create( filename );
+
+            //Start Serialization
+            XmlSerializer xmlSerializer = new XmlSerializer( versionInfo.GetType() );
+            xmlSerializer.Serialize( file, versionInfo );
+            file.Close();
+        }
+
+        /// <summary>
+        /// Loads VersionInfo from specified file.
+        /// </summary>
+        /// <param name="filename">The filename.</param>
+        public void Load ( string filename )
+        {
+            try
+            {
+                //Create a file stream object
+                FileStream file = File.OpenRead( filename );
+
+                //Start Serialization
+                XmlSerializer xmlSerializer = new XmlSerializer( versionInfo.GetType() );
+                versionInfo = ( VersionInfo ) xmlSerializer.Deserialize( file );
+
+                file.Close();
+            }
+            catch ( Exception ex )
+            {
+                throw ex;
+            }
+        }
+    }
+}
