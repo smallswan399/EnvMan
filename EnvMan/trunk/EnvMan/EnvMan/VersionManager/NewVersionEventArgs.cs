9548634eb5986441b8681c09ff4b546e97f5233f
v_setchin <v_setchin@88464f1c-ca2d-0410-b414-c3b21fb8b5ff>
2010-07-03 22:15:31 +0000

    Introduced NewVersionEventArgs class and changed delegate to be as required by Microsoft standards

---
+++ EnvMan/trunk/EnvMan/EnvMan/VersionManager/NewVersionEventArgs.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EnvMan.VersionManager.VersionInformation;
+
+namespace EnvMan.VersionManager
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class NewVersionEventArgs : EventArgs
+    {
+        private bool newVersion = false;
+        /// <summary>
+        /// Gets or sets a value indicating whether [new version].
+        /// </summary>
+        /// <value><c>true</c> if [new version]; otherwise, <c>false</c>.</value>
+        public bool NewVersion
+        {
+            set
+            {
+                newVersion = value;
+            }
+            get
+            {
+                return newVersion;
+            }
+        }
+
+        private VersionInfo versionInfo;
+        /// <summary>
+        /// Gets or sets the version information.
+        /// </summary>
+        /// <value>The version information.</value>
+        public VersionInfo VersionInformation
+        {
+            get
+            {
+                return versionInfo;
+            }
+            set
+            {
+                versionInfo = value;
+            }
+        }
+    }
+}
